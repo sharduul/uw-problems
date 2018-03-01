eb9fab7eebcc3de1288483351b0cd0bc29fdc137
SHARDUL\Shardul <bagadeshardul@gmail.com>
2018-02-28 22:16:56 -0500

    dependencies added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
+import { TaskListComponent } from './task-list/task-list.component';
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+  { path: 'list', component: TaskListComponent },
+  { path: '**', redirectTo: '/list'}
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes
+    ),
+    NgbModule.forRoot(),
+    HttpClientModule,
     BrowserModule
   ],
   providers: [],
---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.less']
+})
+export class TaskListComponent {
+  title = 'app';
+}
