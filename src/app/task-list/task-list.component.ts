de00556548d6c802d6091b737c566ca8205f5244
SHARDUL\Shardul <bagadeshardul@gmail.com>
2018-02-28 22:32:41 -0500

    styles

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import * as _ from "lodash";
+
 
 @Component({
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.less']
 })
 export class TaskListComponent {
-  title = 'app';
+  taskList: any[];
+
+  constructor(private router: Router,
+    private modalService: NgbModal) {
+
+  }
+
+  ngOnInit() {
+    this.taskList = [
+      {
+        name: "Create Angular App"
+      },
+      {
+        name: "Test the app"
+      },
+      {
+        name: "Deploy the app"
+      }
+    ]
+  }
+
 }
